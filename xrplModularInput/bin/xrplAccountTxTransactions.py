e474a26387ec93dce5e3035f6b59adb2aa94b8b9
root <root@number4.localhost>
2023-06-06 23:20:07 +0100

    set up logging and add flag to input for getting full history

--- xrplModularInput/bin/xrplAccountTxTransactions.py
+++ xrplModularInput/bin/xrplAccountTxTransactions.py
@@ -1,4 +1,30 @@
 
+
+### Set up logging
+import sys, os
+import logging, logging.handlers
+import splunk
+def setup_logging():
+    logger = logging.getLogger('splunk.foo')    
+    SPLUNK_HOME = os.environ['SPLUNK_HOME']
+    
+    LOGGING_DEFAULT_CONFIG_FILE = os.path.join(SPLUNK_HOME, 'etc', 'log.cfg')
+    LOGGING_LOCAL_CONFIG_FILE = os.path.join(SPLUNK_HOME, 'etc', 'log-local.cfg')
+    LOGGING_STANZA_NAME = 'python'
+    LOGGING_FILE_NAME = "foo.log"
+    BASE_LOG_PATH = os.path.join('var', 'log', 'splunk')
+    LOGGING_FORMAT = "%(asctime)s %(levelname)-s\t%(module)s:%(lineno)d - %(message)s"
+    splunk_log_handler = logging.handlers.RotatingFileHandler(os.path.join(SPLUNK_HOME, BASE_LOG_PATH, LOGGING_FILE_NAME), mode='a') 
+    splunk_log_handler.setFormatter(logging.Formatter(LOGGING_FORMAT))
+    logger.addHandler(splunk_log_handler)
+    splunk.setupSplunkLogger(logger, LOGGING_DEFAULT_CONFIG_FILE, LOGGING_LOCAL_CONFIG_FILE, LOGGING_STANZA_NAME)
+    return logger
+logger = setup_logging()
+logger.info("hello world!")
+
+###
+
+
 import json
 import sys, os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "lib"))
@@ -33,6 +59,14 @@ class xrplAccountTxTransactions(Script):
         rAddress.required_on_edit = True
         scheme.add_argument(rAddress)
 
+        get_full_history = Argument("get_full_history")
+        get_full_history.title = "Get full account transaction history"
+        get_full_history.data_type = Argument.data_type_boolean
+        get_full_history.description = "get full history description change me"
+        get_full_history.required_on_create = True
+        get_full_history.required_on_edit = False
+        scheme.add_argument(get_full_history)
+
         return scheme
 
 
@@ -41,7 +75,7 @@ class xrplAccountTxTransactions(Script):
         pass
 
 
-    ### Make a function to call the client URL
+    ### Make a function to get the transactions
     def xrplGetData(self,JSON_RPC_URL,rAddress):
         client = JsonRpcClient(JSON_RPC_URL)
         acct=rAddress
@@ -53,11 +87,13 @@ class xrplAccountTxTransactions(Script):
 
     def stream_events(self, inputs, ew):
         # Splunk Enterprise calls the modular input, streams XML describing the inputs to stdin and waits for XML on stfout describing events
-        
+
         for input_name,input_item in inputs.inputs.items():
             JSON_RPC_URL = input_item["JSON_RPC_URL"]
             rAddress = input_item["rAddress"]
-            
+            get_full_history = input_item["get_full_history"]
+            logger.info(get_full_history)
+
             result = self.xrplGetData(JSON_RPC_URL,rAddress)
             print(result)
             for i in result:
